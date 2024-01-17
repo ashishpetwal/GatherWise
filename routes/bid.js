8fe013527d62e4b0ca1c5c4c4821c21958bda530
Ashish <ashishpetwalap2112@gmail.com>
2024-01-17 11:34:11 +0530

    feat: Added Bids listing routes

--- routes/bid.js
+++ routes/bid.js
@@ -2,24 +2,34 @@ const express = require('express');
 const fetchuser = require('../middleware/fetchUser')
 const Bids = require('../models/Bids');
 const Events = require('../models/Events');
+const User = require('../models/User');
 
 const router = express.Router();
 
 router.post('/addnew/:id', fetchuser, async (req, res) => {
     const eventId = req.params.id;
-    const { letter, amount } = req.body;
+    const { letter, amount, service, phone, email } = req.body;
     const bidder = req.user.id;
-    const uniqueBid = await Bids.findOne({ bidder: bidder, event: eventId });
-    const organizer = await Events.findOne({_id:eventId},{organizer:1})
-    if (bidder === organizer.organizer.toString()) {
+    const event = await Events.findOne({ _id: eventId }, { organizer: 1, services: 1 })
+
+    if (bidder === event.organizer.toString()) {
         return res.status(400).json({ success: false, msg: "Organizers can't bid!" })
     }
+
+    const uniqueBid = await Bids.findOne({ bidder: bidder, event: eventId });
+
     if (uniqueBid) {
         return res.status(400).json({ success: false, msg: "You have already submiited the Bid!" })
     }
+
+    if (!event.services.includes(service)) {
+        return res.status(400).json({ success: false, msg: "Invalid Service" })
+    }
+
+    const bidderName = await User.findOne({ _id: bidder }, { name: 1 })
     try {
         const newBid = new Bids({
-            bidder: req.user.id, event: eventId, letter, amount
+            bidder: req.user.id, event: eventId, bidderName: bidderName.name, service, letter, amount, phone, email
         })
         const savedBid = await newBid.save();
 
@@ -29,6 +39,14 @@ router.post('/addnew/:id', fetchuser, async (req, res) => {
     }
 })
 
-
+router.post('/list/:id', fetchuser, async (req, res) => {
+    const eventId = req.params.id;
+    try {
+        const uniqueBids = await Bids.find({ event: eventId });
+        return res.status(200).json(uniqueBids);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+})
 
 module.exports = router;
