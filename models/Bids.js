7395d1d70426d6a0dfb06136751c57136d73f3b5
Ashish <ashishpetwalap2112@gmail.com>
2024-01-17 10:44:56 +0530

    refactor: Changed schema for Bid Data Structure

--- models/Bids.js
+++ models/Bids.js
@@ -1,27 +1,43 @@
 const mongoose = require("mongoose");
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const bidSchema = new Schema ({
-    bidder:{
+const bidSchema = new Schema({
+    bidder: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
-    event:{
+    event: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Events',
+        required: true
+    },
+    bidderName: {
+        type: String,
+        required: true
+    },
+    service: {
+        type: String,
         required: true
     },
     letter: {
         type: String,
         required: true
     },
-    amount:{
+    amount: {
         type: Number,
         required: true
     },
-    submitDate:{
+    phone: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    submitDate: {
         type: Date,
         default: Date.now
     }
