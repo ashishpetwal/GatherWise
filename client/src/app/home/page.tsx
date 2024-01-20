2730a59fb9e298b6160fa0210484f6d9b35a6cfc
Ashish <ashishpetwalap2112@gmail.com>
2024-01-20 22:39:35 +0530

    fix: fixed non-authenticated entry to home route

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -4,12 +4,20 @@ import BModal from '@/components/BModal'
 import BCard from '@/components/Card'
 import Navbar from '@/components/Navbar'
 import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 type Props = {}
 
+type loggedUser = {
+    userType: string
+}
+
 const Home = (props: Props) => {
+
+    const router = useRouter();
+
     const [data, setData] = useState<any[]>([]);
-    const [loggedUser, setLoggedUser] = useState<any[]>();
+    const [loggedUser, setLoggedUser] = useState<loggedUser>();
     const token = localStorage.getItem('token');
 
     const fetchUser = async (token: string) => {
@@ -21,9 +29,10 @@ const Home = (props: Props) => {
         })
         const user = await response.json();
         setLoggedUser(user);
+        fetchEvents(token, user.userType);
     }
-    const fetchEvents = async (token: string) => {
-        const response = await fetch("http://localhost:5000/event/list", {
+    const fetchEvents = async (token: string, user: string) => {
+        const response = await fetch(`http://localhost:5000/event/list/${user}`, {
             method: "GET",
             headers: {
                 auth_token: token
@@ -34,8 +43,12 @@ const Home = (props: Props) => {
     }
 
     useEffect(() => {
-        fetchUser(token || "");
-        fetchEvents(token || "");
+        if (token) {
+            fetchUser(token || "");
+        }
+        else {
+            router.push('login')
+        }
     }, [])
 
 
@@ -46,7 +59,7 @@ const Home = (props: Props) => {
             <div className='flex justify-center'>
                 <div className='sm:grid md:grid-cols-2 lg:grid-cols-3'>
                     {data.map((e) => {
-                        return <BCard key={e._id} id={e._id} title={e.title} desc={e.description} location={e.location} startDate={e.startDate} endDate={e.endDate} services={e.services} />
+                        return <BCard key={e._id} userType={loggedUser?.userType || ""} id={e._id} title={e.title} desc={e.description} location={e.location} startDate={e.startDate} endDate={e.endDate} services={e.services} />
                     })}
                 </div>
             </div>
